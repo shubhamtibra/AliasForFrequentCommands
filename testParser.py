6df4eb5aa605826f36dc373ce107098153e41e5f
Subham Tibra <shubh.tibra@gmail.com>
2024-10-06 20:39:11 +0530

    bug fix in parser

--- testParser.py
+++ testParser.py
@@ -4,18 +4,25 @@ from parseCommand import parse_command_line
 def extract_commands_from_history(history_file):
     with open(history_file, 'r') as file:
         current_command = ""
+        need_match = True
         for line in file:
-            match = re.match(r': \d+:\d+;(.*)', line)
-            if match:
+            command_part = None
+            if need_match:
+                match = re.match(r': \d+:\d+;(.*)', line)
+                if not match:
+                    continue
                 command_part = match.group(1)
-                if current_command:
-                    current_command += " " + command_part
-                else:
-                    current_command = command_part
-                
-                if not current_command.endswith('\\'):
-                    yield current_command.replace('\\', '')
-                    current_command = ""
+            else:
+                command_part = line.strip()
+            if current_command:
+                current_command += " " + command_part
+            else:
+                current_command = command_part
+            need_match = False
+            if not current_command.endswith("\\"):
+                yield current_command.replace("\\", '')
+                current_command = ""
+                need_match = True
 
 history_file = '/Users/subhamtibra/.zsh_history'
 
